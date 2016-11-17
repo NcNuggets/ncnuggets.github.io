ae66d45592c4c6df01949d9c6addb90fec1a0421
ncnuggets <pucillo.nick@gmail.com>
2016-11-16 20:29:00 -0500

    Added fCC RQG files

---
+++ js/fCC-quoteGenerator.js
@@ -0,0 +1,7 @@
+/* 1. need a JSON object which contains quotes
+ * 2. need to add an onclick action to the button
+ * 3. the onclick should randomly serve a quote from the json object
+ * OR
+ * 4. should cycle through a list of quotes
+ * 5. need to tap the twitter api
+ * 6. need to prepare a tweet for sending onclick of twitter btn */
