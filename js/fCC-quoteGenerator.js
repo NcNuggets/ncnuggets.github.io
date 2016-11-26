46f5e85f55baa8075d1a0e4e046fcc7d67d5518e
ncnuggets <pucillo.nick@gmail.com>
2016-11-25 22:36:12 -0500

    finished html and css

--- js/fCC-quoteGenerator.js
+++ js/fCC-quoteGenerator.js
@@ -1,7 +1,28 @@
 /* 1. need a JSON object which contains quotes
  * 2. need to add an onclick action to the button
  * 3. the onclick should randomly serve a quote from the json object
- * OR
- * 4. should cycle through a list of quotes
  * 5. need to tap the twitter api
  * 6. need to prepare a tweet for sending onclick of twitter btn */
+
+function main () {
+  // variables
+  var quoteNumber = 0;
+  var newQuote = "test quote";
+  var newAuthor = "test author";
+
+  // object constructor to build quotes called "Quotes"
+  function Quotes (quote, author) {
+    this.quote = quote;
+    this.author = author;
+  }
+
+  // function to randomize a number between 0 - X, where X is the size of object Quotes, and assigns that number to quoteNumber
+
+  // function that chooses a quote from quotes and assigns it to array newQuote
+
+  // function that changes the html id's for quoteText and quoteAuthor to the proper indeces from newQuote
+  function setQuote (newQuote, newAuthor) {
+    document.getElementsById('quoteText').innerHTML = newQuote;
+    document.getElementsById('quoteAuthor').innerHTML = newAuthor;
+  }
+}
