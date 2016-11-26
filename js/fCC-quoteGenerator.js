37f6409f926b19e2e11fee848ca8cd25b3a0f16d
ncnuggets <pucillo.nick@gmail.com>
2016-11-26 02:29:55 -0500

    random quote generator completed

--- js/fCC-quoteGenerator.js
+++ js/fCC-quoteGenerator.js
@@ -18,10 +18,8 @@ quotes = [
   ['- Thomas A. Edison', 'I have not failed. I\'ve just found 10,000 ways that won\'t work.'],
   ['- Winston Churchill', 'Success is stumbling from failure to failure with no loss of enthusiasm.'],
   ['- John Green', 'What is the point of being alive if you don\'t at least try to do something remarkable?'],
-  ['- Albert Einstein', 'Reading, after a certain age, diverts the mind too much from its creative pursuits. Any man who reads too much and uses his own brain too little falls into lazy habits of thinking.'],
   ['- Jeff Bezos', 'We are stubborn on vision, but flexible on the details..'],
-  ['- Arnold Schwarzenegger', 'I am not a self-made man.'],
-  ['- George Bernard Shaw', 'The reasonable man adapts himself to the conditions that surround him... The unreasonable man adapts surrounding conditions to himself... All progress depends on the unreasonable man.']
+  ['- Arnold Schwarzenegger', 'I am not a self-made man.']
 ];
 
 // assigning the quote and author variables with the html ids
@@ -56,10 +54,8 @@ tweets = [
   'https://twitter.com/intent/tweet?text="I have not failed. I\'ve just found 10,000 ways that won\'t work." Thomas A. Edison',
   'https://twitter.com/intent/tweet?text="Success is stumbling from failure to failure with no loss of enthusiasm." Winston Churchill',
   'https://twitter.com/intent/tweet?text=https://twitter.com/intent/tweet?text="What is the point of being alive if you don\'t at least try to do something remarkable?" John Green',
-  'https://twitter.com/intent/tweet?text="Reading, after a certain age, diverts the mind too much from its creative pursuits. Any man who reads too much and uses his own brain too little falls into lazy habits of thinking." Albert Einstein',
   'https://twitter.com/intent/tweet?text="We are stubborn on vision, but flexible on the details.." Jeff Bezos',
-  'https://twitter.com/intent/tweet?text="I am not a self-made man." Arnold Schwarzenegger',
-  'https://twitter.com/intent/tweet?text="The reasonable man adapts himself to the conditions that surround him... The unreasonable man adapts surrounding conditions to himself... All progress depends on the unreasonable man." George Bernard Shaw'
+  'https://twitter.com/intent/tweet?text="I am not a self-made man." Arnold Schwarzenegger'
 ];
 
 // function to set the tweet
