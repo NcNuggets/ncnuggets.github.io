8fbbcd6124e9d20d3dca5a53230beb7ec57733a2
ncnuggets <pucillo.nick@gmail.com>
2016-11-26 01:28:12 -0500

    random quote generator done

--- js/fCC-quoteGenerator.js
+++ js/fCC-quoteGenerator.js
@@ -5,22 +5,47 @@
  * 6. need to prepare a tweet for sending onclick of twitter btn */
 
 // variables
-var newQuote = "test quote 3";
-var newAuthor = "test author 3";
-var randNum = 0;
+var oldQuote = "";
+var oldAuthor = "";
+var newQuote = "";
+var newAuthor = "";
+var quotes = [];
+var quoteNum = 0;
 
-var oldQuote = document.getElementById('quoteText');
-var oldAuthor = document.getElementById('quoteAuthor');
+// array of quotes
+quotes = [
+  ['- Janet Fitch', 'The phoenix must burn to emerge.'],
+  ['- Henry Ford', 'The only real mistake is the one from which we learn nothing.'],
+  ['- Thomas A. Edison', 'I have not failed. I\'ve just found 10,000 ways that won\'t work.'],
+  ['- Winston Churchill', 'Success is stumbling from failure to failure with no loss of enthusiasm.'],
+  ['- John Green', 'What is the point of being alive if you don\'t at least try to do something remarkable?'],
+  ['- Albert Einstein', 'Reading, after a certain age, diverts the mind too much from its creative pursuits. Any man who reads too much and uses his own brain too little falls into lazy habits of thinking.'],
+  ['- Jeff Bezos', 'We are stubborn on vision, but flexible on the details..'],
+  ['- Arnold Schwarzenegger', 'I am not a self-made man.'],
+  ['- George Bernard Shaw', 'The reasonable man adapts himself to the conditions that surround him... The unreasonable man adapts surrounding conditions to himself... All progress depends on the unreasonable man.']
+];
 
-oldQuote.innerHTML = newQuote;
-oldAuthor.innerHTML = newAuthor;
+// assigning the old quotes with the html ids
+oldQuote = document.getElementById('quoteText');
+oldAuthor = document.getElementById('quoteAuthor');
 
-// object constructor to build quotes called "Quotes"
-function Quotes (quote, author) {
-  this.quote = quote;
-  this.author = author;
+// create a random integer for quoteNum
+function randInt() {
+  min = Math.ceil(0);
+  max = Math.floor(quotes.length);
+  return Math.floor(Math.random() * (max - min)) + min;
 }
 
-// function to randomize a number between 0 - X, where X is the size of object Quotes, and assigns that number to quoteNumber
+quoteNum = randInt();
 
-// function that chooses a quote from quotes and assigns it to array newQuote
+// loading the default quote in the array
+oldQuote.innerHTML = quotes[quoteNum][1];
+oldAuthor.innerHTML = quotes[quoteNum][0];
+
+// function to randomize a number between 0 - X, where X is the size of quotes, and assigns that number to randNum
+function nextQuote() {
+  randInt();
+  quoteNum = randInt();
+  oldQuote.innerHTML = quotes[quoteNum][1];
+  oldAuthor.innerHTML = quotes[quoteNum][0];
+}
