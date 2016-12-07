32ec7e79756d52743fa8f5f128f86469edb96d9b
ncnuggets <pucillo.nick@gmail.com>
2016-12-07 03:57:34 -0500

    started weatherApp

---
+++ fCC/fCC-weatherApp/js/fCC-weatherApp.js
@@ -0,0 +1,13 @@
+/**************************
+* Need to see weather in current location
+*   - need to read user location
+*   - set user location to variable
+*   - pull current weather for location variable
+*   - display necessary parts of the weather api call
+* Need to display weather specific icons or backgrounds
+*   - use the weather group method to set image
+* Button to toggle between Fahrenheit or Celcius
+***************************/
+
+var userLat = "";
+var userLon = "";
