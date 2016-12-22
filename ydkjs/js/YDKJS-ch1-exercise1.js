8926ffb8d5ea5b211862b18def111270799f4654
ncnuggets@github.com <pucillo.nick@gmail.com>
2016-12-22 12:25:31 -0500

    added exercise case instructions

--- ydkjs/js/YDKJS-ch1-exercise1.js
+++ ydkjs/js/YDKJS-ch1-exercise1.js
@@ -1,5 +1,21 @@
+/*
+Calculate total cost of purchasing phones.
+Keep purchasing phones until you run out of money in your bank account.
+Buy accessories for each phone as long as your purchase amount is below your mental spending threshold.
+Add in the tax.
+Print out the calculated purchase amount, properly formatted.
+Check the amount against your bank account balance to see if you can afford it or not.
+
+You should set up some constants for the "tax rate," "phone price," "accessory price," and "spending threshold," as well as a variable for your "bank account balance."
+
+You should define functions for calculating the tax and for formatting the price with a "$" and rounding to two decimal places.
+
+Bonus Challenge: Try to incorporate input into this program, perhaps with the prompt(..) covered in "Input" earlier. You may prompt the user for their bank account balance, for example. Have fun and be creative!
+*/
+
 function calculatePhoneCost() {
-  var balance = prompt('Please enter a sample bank account balance');
+  var balance = prompt("Please enter a sample bank account balance");
+  const  TAX_RATE = 0.06
 
   console.log(balance);
 }
