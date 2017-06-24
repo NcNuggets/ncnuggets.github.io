84a4ab6c86849295ca24442665b054c7bdfcbacb
NcNuggets <pucillo.nick@gmail.com>
2017-06-24 14:09:54 -0400

    json test failed, edits made

--- card-game/js/card_game.js
+++ card-game/js/card_game.js
@@ -1,5 +1,5 @@
 function cardGame() {
-  var cardData = JSON.parse(data);
+  var cardData = JSON.parse(cardDeck);
   alert(cardData[0].name);
   alert(cardData[0].label);
   alert(cardData[0].cardNum);
