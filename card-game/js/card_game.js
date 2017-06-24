cb9a9863a2de1a52552814a4c5df57a69b7165ea
NcNuggets <pucillo.nick@gmail.com>
2017-06-24 13:50:56 -0400

    json success, added card data

--- card-game/js/card_game.js
+++ card-game/js/card_game.js
@@ -1,6 +1,11 @@
-function load() {
-  var mydata = JSON.parse(data);
-  alert(mydata[0].name);
+function cardGame() {
+  var cards = JSON.parse(cards);
+  console.log(cards[0].name);
+  console.log(cards[0].label);
+  console.log(cards[0].cardNum);
+  console.log(cards[0].value);
+  console.log(cards[0].suit);
+  console.log(cards[0].color);
   alert(mydata[0].age);
   alert(mydata[1].name);
   alert(mydata[1].age);
