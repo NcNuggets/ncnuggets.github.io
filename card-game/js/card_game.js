e74eb217d4a5488dad240c5b0e1e4c14d5fe302a
NcNuggets <pucillo.nick@gmail.com>
2017-06-24 13:53:12 -0400

    json test 5

--- card-game/js/card_game.js
+++ card-game/js/card_game.js
@@ -1,12 +1,12 @@
 function cardGame() {
-  var cards = JSON.parse(cards);
+  var cardData = JSON.parse(cards);
   console.log(cards[0].name);
   console.log(cards[0].label);
   console.log(cards[0].cardNum);
   console.log(cards[0].value);
   console.log(cards[0].suit);
   console.log(cards[0].color);
-  alert(mydata[0].age);
-  alert(mydata[1].name);
-  alert(mydata[1].age);
+  // alert(mydata[0].age);
+  // alert(mydata[1].name);
+  // alert(mydata[1].age);
 }
