1ab2dc1f2e94e86b055413a47e7c3cebe27010a0
NcNuggets <pucillo.nick@gmail.com>
2017-06-24 13:05:04 -0400

    updated file name convention and json test

---
+++ card-game/js/card_game.js
@@ -0,0 +1,10 @@
+/*
+var mydata = JSON.parse(data);
+alert(mydata[0].name);
+alert(mydata[0].age);
+alert(mydata[1].name);
+alert(mydata[1].age);
+*/
+
+var cardData = JSON.parse(cards);
+alert(cardData.spades.A.label);
